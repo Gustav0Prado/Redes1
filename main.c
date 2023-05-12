e5260b053df48cebde969bb95551675d1fe965ef
Gustavo <guprado.silva@gmail.com>
2023-05-12 17:59:01 -0300

    [del] remove loopback

--- main.c
+++ main.c
@@ -10,13 +10,7 @@ int main(){
    unsigned char rcve[256];
    char lixo;
 
-   #ifdef DEBUG
-   int socket = ConexaoRawSocket("lo");
-   int test = 0;
-
-   #else
-   int socket = ConexaoRawSocket("enp3s0");
-   #endif
+   int socket = ConexaoRawSocket("eno1");
 
    printf("0 - Cliente\n1 - Servidor\n");
    scanf("%d", &servidor);
@@ -26,21 +20,10 @@ int main(){
 
    if(servidor){
       while(1){
-         #ifdef DEBUG
-         if(recv(socket, rcve, 256, 0) != -1){
-            if(test % 2 == 0){
-               printf("%s", rcve);
-               memset(rcve, 0, 256);
-            }
-            test++;
-         }
-
-         #else
          if(recv(socket, rcve, 256, 0) != -1){
             printf("%s", rcve);
             memset(rcve, 0, 256);
          }
-         #endif
       }
    }
    else{
