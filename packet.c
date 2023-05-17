89ff38cd9731008450555267cf45fff1c40a5d4d
Gustavo P <guprado.silva@gmail.com>
2023-05-17 11:39:53 -0300

    [fix] copiar os campos 1 a 1 ao inves de usar memcpy funciona corretamente [fix] gitignore ignora teste de pacote

--- packet.c
+++ packet.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 int main(){
-    unsigned char pacoteChar[4];
-    pacote_t pacote, pacote2;
+    unsigned char pacoteChar[5];
+    pacote_t pacote;
     pacote.marcador  = 126;
-    pacote.tamanho   = 0;
-    pacote.sequencia = 0;
-    pacote.tipo      = 2;
-    pacote.paridade  = 0;
+    pacote.tamanho   = 15;
+    pacote.sequencia = 20;
+    pacote.tipo      = 4;
+    pacote.paridade  = 13;
 
     for(int i = 7; i >= 0; i--) putchar('0' + ((pacote.marcador >> i) & 1));
     printf(" ");
@@ -26,16 +26,30 @@ int main(){
     for(int i = 7; i >= 0; i--) putchar('0' + ((pacote.paridade >> i) & 1));
     printf("\n");
 
-    memcpy(pacoteChar, &pacote, 4);
-    memcpy(&pacote2, pacoteChar, 4);
+    pacoteChar[0] = pacote.marcador;
+    pacoteChar[1] = pacote.tamanho;
+    pacoteChar[2] = pacote.sequencia;
+    pacoteChar[3] = pacote.tipo;
+    pacoteChar[4] = pacote.paridade;
     
+    for(int i = 0; i < sizeof(pacoteChar); ++i){
+        printf("%d ", pacoteChar[i]);
+    }
+    printf("\n");
+
     for(int i = 7; i >= 0; i--) putchar('0' + ((pacoteChar[0] >> i) & 1));
     printf(" ");
 
     for(int i = 5; i >= 0; i--) putchar('0' + ((pacoteChar[1] >> i) & 1));
     printf(" ");
 
-    for(int i = 1; i >= 0; i--) putchar('0' + ((pacoteChar[1] >> i) & 1));
+    for(int i = 5; i >= 0; i--) putchar('0' + ((pacoteChar[2] >> i) & 1));
+    printf(" ");
+
+    for(int i = 3; i >= 0; i--) putchar('0' + ((pacoteChar[3] >> i) & 1));
+    printf(" ");
+
+    for(int i = 7; i >= 0; i--) putchar('0' + ((pacoteChar[4] >> i) & 1));
     printf(" ");
 
     printf("\n");
