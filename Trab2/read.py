431305e031c546712b89186acd2e991990ad6715
Gustavo <guprado.silva@gmail.com>
2023-05-19 19:03:28 -0300

    [add] lista de conexoes e class Maquina

--- Trab2/read.py
+++ Trab2/read.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
 
-conexoes = {}
+import socket
+
+class Maquina:
+   def __init__(self, ip, porta):
+      self.ip = ip
+      self.porta = porta
+   
+   def proximaMaq(self):
+
+conexoes = []
 with open("conf.txt") as f:
-   qtdMaquinas = f.readline()
+   qtdMaquinas = int(f.readline())
    while True:
       line = f.readline()
       if not line:
          break
+
       entrada = line.split()
       ip = entrada[0]
       porta = entrada[1]
-      conexoes[ip] = porta
+
+      conexoes.append(ip)
+      conexoes.append(porta)
    print(conexoes)
 
-mAtual = 
+IpAtual = socket.gethostbyname(socket.gethostname())
