55bbc3d8649979237111468b98eef160917d17a1
Gustavo P <guprado.silva@gmail.com>
2023-06-08 18:10:13 -0300

    [add] teste com 8 bytes

--- Trab2/read.py
+++ Trab2/read.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 
-import socket, random
+import socket
 import sys, termios
+import random
 
 ips = []
 portas = []
 personalDeck = []
-bastao = 0
-hostId = ips.index(socket.gethostname())
+bastao = False
+hostId = 0
+
+class Mensagem:
+  def __init__(self, inicio, origem, tipo, jogada, confirmacao, fim):
+      self.inicio = inicio
+      self.origem = origem
+      self.tipo = tipo
+      self.jogada = jogada
+      self.confirmacao = confirmacao
+      self.fim = fim
+      return
+  def __str__(self):
+     return self.inicio + str(self.origem) + self.tipo + str(self.jogada) + str(self.confirmacao) + self.fim
 
 
 def init_deck (deck):
@@ -26,7 +39,7 @@ def init_deck (deck):
    random.shuffle(deck)
 
 
-def the_deal(deck, playersNum):
+def the_deal(deck, playersNum, sender, listener):
    """Cartear baralho
 
    Args:
@@ -34,14 +47,18 @@ def the_deal(deck, playersNum):
       playersNum (int): Quantidade de jogadores na partida
    """
    card = 0
-   i = 0
    while (len(deck) > 0):
-      card = deck.pop()
-      if(i == 0):
-         personalDeck.append(card)
-      #else:
-         #send(cria_mensagem(str(i)+"c,"+str(card)))
-      i = (i + 1) % playersNum
+      for i in range (playersNum):
+         if (len(deck) < 0):
+            return
+   
+         card = deck.pop()
+
+         if (i == hostId):
+            personalDeck.append(card)
+         else:
+            # cd == card deal
+            send(f"({hostId}cd{i}{card}00000000)", playersNum, sender, listener)
 
 
 def check_confirm(message, playersNum):
@@ -52,11 +69,10 @@ def check_confirm(message, playersNum):
       playersNum (int): Quantidade de jogadores na partida
 
    Returns:
-      Bool: True caso correto ou False caso haja erro
+      bool: True caso correto ou False caso haja erro
    """
-   
-   confirm = message[-2]
-   if (bin(confirm).count("1") != playersNum):
+   confirm = message[-9:-1]
+   if (confirm.count("1") != playersNum):
       return False
    return True
     
@@ -66,126 +82,155 @@ def flip_bit(value, n):
    """Flipa o n-ésimo bit (da direita pra esquerda) de value
 
    Args:
-      value (int): Valor inteiro
+      value (string): Valor em bits
       n (int): Posição do bit a ser flipado
 
    Returns:
       int: Valor com bit já flipado
    """
-
-   if (bin(value)[n+2] == '0'):
-      return value | (1 << n)
+   if (value[n] == '0'):
+      return value[:n] + '1' + value[n+1:]
    else:
-      return value & ~(1 << n)
-   
+      return value[:n] + '0' + value[n+1:]
+    
 
 
-def receive (message):
-   """Recebe mensagem e marca campo de confirmação como recebido
-
-   Args:
-       message (string): Mensagem recebida
-   """
-   
-   rec_data, addr = listen.recvfrom(1024)
-   message = flip_bit(rec_data[-2], hostId)
-
-
-
-def send (message):
-   """Envia mensagem pela rede. Caso alguém do anel tenha tido erro, reenvia a mensagem
+def send (message, playersNum, sender, listener):
+   """Envia mensagem pelo anel até receber de volta e confirmar o recebimento
 
    Args:
-      message (string): Mensagem a ser enviada
+       message (string): Mensagem a ser enviada
+       playersNum (int): Número de jogadores
+       sender (socket): Socket de envio
+       listener (socket): Socket local de recebimento
    """
-   received = ''
-   s.sendto(message.encode(), (ips[ (hostId+1) % qtd  ], int(portas[ (hostId+1) % qtd ])) )
-   receive(received)
-   if (not (check_confirm(received))) :
-      send(message)
+   check = False
+   i = 0
+   # Se tiver bastao, mensagem roda o anel
+
+   if bastao:
+      while(check == False and i < 100):
+         sender.sendto(message.encode(), (ips[ (hostId+1) % playersNum  ], int(portas[ (hostId+1) % playersNum ])) )
+         rec_data, addr = listener.recvfrom(1024)
+
+         rec_data = rec_data.decode()
+
+         print( "rec_data é" + rec_data)
+         confirmation = rec_data[-9:-1]
+         
+         print ("confirmation antes é " + bin(confirmation))
+         confirmation = flip_bit(confirmation, hostId)
+         
+         print ("confirmation depois é " + bin(confirmation))
+         rec_aux = rec_data[:-9] + confirmation + rec_data[-1] 
+
+         check = check_confirm(rec_aux, playersNum)
+         if not check:
+            print("Mensagem não recebida, mandando novamente")
+         i += 1
+      # não confirmou recebimento da mensagem
+      assert i < 100
+   # sem bastao, só repassa a mensagem
+   else:
+      sender.sendto(message.encode(), (ips[ (hostId+1) % playersNum  ], int(portas[ (hostId+1) % playersNum ])) )
+
+def receive (sender, listener, playersNum):
+   global bastao
+
+   rec_data, addr = listener.recvfrom(1024)
+   rec_data = rec_data.decode()
+   rec_msg = Mensagem(rec_data[0], rec_data[1], rec_data[2:4], rec_data[4:-9], int(rec_data[-9:-1]), rec_data[-1])
+
+   if(rec_msg.inicio == '('  and  rec_msg.fim == ')'):
+      # token pass
+      if(rec_msg.tipo   == "tp"):
+         bastao = True
+      # card deal
+      elif(rec_msg.tipo == "cd"):
+         if(int(rec_msg.jogada[0]) == hostId):
+            personalDeck.append(int(rec_msg.jogada[1:]))
+
+         rec_msg.confirmacao = flip_bit(rec_msg.confirmacao, hostId)
+         print(rec_msg.confirmacao)
+         send(str(rec_msg), playersNum, sender, listener)
+      # hand discard
+      elif(rec_msg.tipo == "hd"):
+         print ("faz hd")
+
+      # simple pass
+      elif(rec_msg.tipo == "sp"):
+         print ("faz sp")
 
 
-def pass_on(message):
-   """Repassa mensagem para o próximo do anel
 
-   Args:
-       message (string): Mensagem a ser repassada
-   """
-   s.sendto(message, (ips[ (hostId+1) % qtd   ], int(portas[ (hostId+1) % qtd ])) )
+def main():
+   global hostId
 
+   # Le arquivo de configuracao e coloca nomes das maquinas e portas em listas
+   with open("conf.txt") as f:
+      playersNum = int(f.readline())
+      while True:
+         line = f.readline()
+         if not line:
+            break
 
-def passa_bastao():
-   """Passa o bastão para a próxima máquina
-   """
-   #send(cria_mensagem("b"))
-   bastao = False
+         entrada = line.split()
+         ips.append(entrada[0])
+         portas.append(entrada[1])
 
+   hostId = ips.index(socket.gethostname())
+   print(hostId)
 
-def process(message):
-   """Processa a mensagem recebida
+   # Escuta pacotes vindos do nó anterior na porta atual
+   listen = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+   listen.bind( (socket.gethostbyname(ips[hostId]), int(portas[hostId])) )
+   print("Listening on ", portas[hostId])
 
-   Args:
-      message (string): Mensagem a ser processada
-   """
+   # Envia pacotes para o próximo nó
+   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+   print("Sending to", ips[ (hostId+1) % playersNum ], "on port", portas[ (hostId+1) % playersNum ])
 
-   if(message.decode()[2] == 'b'):
+   # Primeira maquina do arquivo começa com o bastão
+   global bastao
+   if(hostId == 0):
       bastao = True
-   elif(message.decode()[2] == 'p'):
-      #jogador passou a vez
-      pass_on(received)
-   elif(message.decode()[2] == 'j'):
-      #processa jogada
-      pass_on(received)
 
-# Le arquivo de configuracao e coloca nomes das maquinas e portas em listas
-with open("conf.txt") as f:
-   qtd = int(f.readline())
-   while True:
-      line = f.readline()
-      if not line:
-         break
+   #while(True):
+      # # Se tiver o bastão, envia mensagem, espera receber de volta e passa o bastão pra frente
+      # if(bastao):
+      #    termios.tcflush(sys.stdin, termios.TCIOFLUSH)
+      #    send_data = input("\t>>> Você possui o bastao, envie uma mensagem: ")
+      #    s.sendto(send_data.encode(), (ips[ (hostId+1) % playersNum  ], int(portas[ (hostId+1) % playersNum ])) )
 
-      entrada = line.split()
-      ips.append(entrada[0])
-      portas.append(entrada[1])
+      #    rec_data, addr = listen.recvfrom(1024)
 
-hostId = ips.index(socket.gethostname())
-
-# Escuta pacotes vindos do nó anterior na porta atual
-listen = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-listen.bind( (socket.gethostbyname(ips[hostId]), int(portas[hostId])) )
-
-# Envia pacotes para o próximo nó
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# Primeira maquina do arquivo começa com o bastão
-if(hostId == 0):
-   bastao = True
-
-while(True):
-   # Se tiver o bastão, envia mensagem, espera receber de volta e passa o bastão pra frente
-   if(bastao):
-      termios.tcflush(sys.stdin, termios.TCIOFLUSH)
-      send_data = input("\t>>> Você possui o bastao, envie uma mensagem: ")
-      # s.sendto(send_data.encode(), (ips[ (hostId+1) % qtd  ], int(portas[ (hostId+1) % qtd ])) )
-
-      # rec_data, addr = listen.recvfrom(1024)
-
-      # s.sendto(b'b', (ips[ (hostId+1) % qtd   ], int(portas[ (hostId+1) % qtd ])) )
-      # bastao = False
-      send(send_data.encode())
-      passa_bastao()
-   # Se nao tiver o bastao espera uma mensagem
-   else:
-      # print("\tVocê nao possui o bastao, aguarde uma mensagem")
-      # rec_data, addr = listen.recvfrom(1024)
-      # if(rec_data.decode() == 'b'):
-      #    bastao = True
+      #    s.sendto(b'b', (ips[ (hostId+1) % playersNum   ], int(portas[ (hostId+1) % playersNum ])) )
+      #    bastao = False
+      # # Se nao tiver o bastao espera uma mensagem
       # else:
-      #    print("Mensagem recebida: ", rec_data.decode())
-      #    s.sendto(rec_data, (ips[ (hostId+1) % qtd   ], int(portas[ (hostId+1) % qtd ])) )
-      received = ''
-      receive(received)
-      process(received)
-      
+      #    print("\tVocê nao possui o bastao, aguarde uma mensagem")
+      #    rec_data, addr = listen.recvfrom(1024)
+      #    if(rec_data.decode() == 'b'):
+      #       bastao = True
+      #    else:
+      #          print("Mensagem recebida: ", rec_data.decode())
+      #          s.sendto(rec_data, (ips[ (hostId+1) % playersNum   ], int(portas[ (hostId+1) % playersNum ])) )
+   
+   if(hostId == 0):
+      print("Dealing Cards")
+      deck = []
+      init_deck(deck)
+      the_deal(deck, playersNum, s, listen)
+   else:
+      if (hostId < 80 % playersNum):
+         valorWhile =  (80 // playersNum) + 1
+      else:
+         valorWhile =  80//playersNum
+      #while(len(personalDeck) < valorWhile):
+      for i in range(60):
+         receive(s, listen, playersNum)
+   
+   print(personalDeck)
 
+if __name__ == "__main__":
+      main()
