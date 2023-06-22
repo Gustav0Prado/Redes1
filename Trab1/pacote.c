07f4af95ed10c660d3248faddf909ec099665fba
Gustavo Prado <guprado.silva@gmail.com>
2023-06-22 09:05:26 -0300

    [add] Barra de progresso + diminui sleep

--- Trab1/pacote.c
+++ Trab1/pacote.c
@@ -1,5 +1,12 @@
 #include "utils.h"
 
+void clearLines(){
+   printf("\033[1A");
+   for (int i = 0; i < 100; ++i){
+      printf("\b");
+   }
+}
+
 /**
  * @brief Divide e envia o arquivo indicado
  * 
@@ -9,13 +16,15 @@ void enviaArquivo(int socket, char *arquivo, int *seq){
    FILE* arq;
    struct stat st;
    unsigned char buff[67];
-   int tam_read;
+   int tam_read, progress = 0;
 
    buff[0] = 126;
    buff[3] = 5;
 
    stat(arquivo, &st);
    long tamanho = st.st_size;
+
+   printf("\t%d%%\n", progress);
 
    arq = fopen(arquivo, "r");
    if(arq == NULL){
@@ -26,10 +35,14 @@ void enviaArquivo(int socket, char *arquivo, int *seq){
       tam_read = fread(buff+4, sizeof(unsigned char), 63, arq);
       buff[1] = tam_read;
       buff[2] = *seq;
-      printf("%d\n", buff[2]);
+
+      //printf("%d\n", buff[2]);
+      clearLines();
+      printf("\tEnviando... %.2f%%\n", (++progress/ (float) (tamanho/63)) * 100);
+      
       send(socket, buff, 67, 0);
       (*seq) = ((*seq) + 1) % 256;
-      usleep(30000);
+      usleep(1);
    }
 
    tam_read = fread(buff+4, sizeof(unsigned char), tamanho%63, arq);
@@ -37,10 +50,12 @@ void enviaArquivo(int socket, char *arquivo, int *seq){
       tam_read = fread(buff+4, sizeof(unsigned char), tamanho%63, arq);
       buff[1] = tam_read;
       buff[2] = *seq;
-      printf("%d\n", buff[2]);
+      
+      //printf("%d\n", buff[2]);
+
       send(socket, buff, 67, 0);
       (*seq) = ((*seq) + 1) % 256;
-      usleep(30000);
+      usleep(1);
    }
 
    fclose(arq);
