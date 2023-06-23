4b3c2c4bb0abe0492f9a88506ca219f3ae67a837
camaleoao <camargo.leonardo1@gmail.com>
2023-06-23 19:05:54 +0000

    [add] Struct sequencia

--- Trab1/pacote.h
+++ Trab1/pacote.h
@@ -5,5 +5,11 @@ typedef struct  __attribute__((packed, scalar_storage_order("big-endian"))) {
    unsigned int tipo : 4;
 } pacote_t;
 
+typedef struct seq_t
+{
+    unsigned int client:6;
+    unsigned int server:6;
+}seq_t;
+
 void enviaArquivo(int socket, char *arquivo, int *seq);
 void escreveParte(char *arquivo, unsigned char *dados, int tamanho);
